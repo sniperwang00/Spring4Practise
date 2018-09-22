607800a6ca669aa9e18049d93d5016220d3e7b1a
Wang Luke <sniperwang00@gmail.com>
2018-09-22 23:23:14 +0800

    init

--- src/main/java/spittr/config/RootConfig.java
+++ src/main/java/spittr/config/RootConfig.java
@@ -1,4 +1,9 @@
 package spittr.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spittr")
 public class RootConfig {
 }
--- src/main/java/spittr/config/SpittrWebAppInitializer.java
+++ src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -1,4 +1,17 @@
 package spittr.config;
 
-public class SpittrWebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
